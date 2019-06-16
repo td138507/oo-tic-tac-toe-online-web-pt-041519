32903821d98e01eb08c1e131da8ac2ae538e111b
Taylor Doyle <tcd56@cornell.edu>
2019-06-16 22:46:32 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,50 @@
+class TicTacToe
+  def initialize
+    @board = array.new(9," ")
+  end
+
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+def display_board
+  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+  puts "---------"
+  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+  puts "---------"
+  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+end
+
+def input_to_index(input)
+  input.to_i - 1
+end
+
+def move(index, token = "X")
+  @board[index] = token
+end
+
+def position_taken?(index)
+  @board[index] != " " ? true : false
+end
+
+def valid_move?(index)
+  index.between?(0,8) && !position_taken?(index)
+end
+
+def turn_count
+  number_of_turns = 0
+  @board.each do |token|
+    if token == "X" || token == "O"
+      number_of_turns += 1
+    end
+  end
+  number_of_turns
+end
+
